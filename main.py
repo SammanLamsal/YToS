97d3b8d490483d52d78566136e72b1390f89f730
sslokk <slok123low@gmail.com>
2022-01-01 12:49:50 -0500

    Made download directory changable

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from posixpath import abspath
 from tkinter.constants import CENTER
 import yt_dlp as ytdl
 import os, sys
@@ -6,10 +7,11 @@ from tkinter import filedialog, Text
 
 from yt_dlp.utils import DownloadError
 
+absolutePath = ""
 
 ydl_opts = {
     'format': 'bestaudio/best',
-    'outtmpl': f"C:/Users/slok1/Desktop/Songs/%(title)s.%(ext)s",
+    'outtmpl': '%(title)s.%(ext)s',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -20,12 +22,17 @@ ydl_opts = {
 linkEntry = ""
 
 def downloadSong():
-    if(DownloadError):
-        canvas.create_text(398, 450, text = "INVALID INPUT, TRY AGAIN")
     with ytdl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([linkEntry.get()])
+        try:
+            ydl.download([linkEntry.get()])
+        except:
+            canvas.create_text(398, 450, text = "INVALID INPUT, TRY AGAIN")
 
-
+def browseFolders():
+    folderName = filedialog.askdirectory(initialdir = "/", title = "SELECT A FOLDER")
+    absolutePath = os.path.abspath(folderName)
+    ydl_opts['outtmpl'] = absolutePath + ydl_opts['outtmpl']
+    print(absolutePath)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -40,7 +47,12 @@ if __name__ == "__main__":
     canvas.create_window(200, 140, window=linkEntry)
     linkEntry.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-    linkText = canvas.create_text(398, 350, anchor = CENTER, text = "INPUT PLAYLIST LINK")
+    chooseDlLocationTxt = canvas.create_text(398, 500, anchor = CENTER, text = "CHOOSE DOWNLOAD LOCATION")
+    browseButton = tk.Button(canvas,text = "BROWSE FOLDERS", command = browseFolders)
+    browseButton.place(x=360, y=525)
+
+
+    linkTxt = canvas.create_text(398, 360, anchor = CENTER, text = "INPUT PLAYLIST LINK")
 
     download = tk.Button(root, text = "DOWNLOAD SONG", padx=10, pady=5, fg="white", bg="#263D42", command = downloadSong)
     download.pack()
