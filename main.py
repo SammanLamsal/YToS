a3c1b7bf155503a952a22e3841ae9d5ca0cf9521
sslokk <slok123low@gmail.com>
2021-12-31 22:58:05 -0500

    GUI implementation

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from tkinter.constants import CENTER
 import yt_dlp as ytdl
 import os, sys
+import tkinter as tk
+from tkinter import filedialog, Text
+
+from yt_dlp.utils import DownloadError
+
 
 ydl_opts = {
     'format': 'bestaudio/best',
@@ -11,5 +17,33 @@ ydl_opts = {
     }],
 }
 
-with ytdl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(["https://www.youtube.com/playlist?list=PLPKA8OH9NAHu8iXKd28AFqiPQe5xqbIDJ"])
+linkEntry = ""
+
+def downloadSong():
+    if(DownloadError):
+        canvas.create_text(398, 450, text = "INVALID INPUT, TRY AGAIN")
+    with ytdl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([linkEntry.get()])
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("YTOS")
+    root.geometry("800x800")
+    root.resizable(height = None, width = None)
+
+    canvas = tk.Canvas(root, bg = "#263")
+    canvas.pack(fill="both", expand="true")
+    
+    linkEntry = tk.Entry(root, justify=CENTER)
+    canvas.create_window(200, 140, window=linkEntry)
+    linkEntry.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    linkText = canvas.create_text(398, 350, anchor = CENTER, text = "INPUT PLAYLIST LINK")
+
+    download = tk.Button(root, text = "DOWNLOAD SONG", padx=10, pady=5, fg="white", bg="#263D42", command = downloadSong)
+    download.pack()
+
+    root.mainloop()
+
